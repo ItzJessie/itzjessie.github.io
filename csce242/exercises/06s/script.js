32745ed263b370abb4436c551e3fc4bc21e0d907
cse-brownjessiej <jessiejb@email.sc.edu>
2025-02-03 19:07:54 -0500

    javascript example

---
+++ csce242/exercises/06s/script.js
@@ -0,0 +1,10 @@
+//2-3-25
+// 1. Wait until the document is fully loaded
+// 2. Use the `onload` event listener to execute the code
+// 3. Then do what is in this function
+
+window.onload = () => {
+   document.write("hello, world!");
+};
+
+//document.write("hello, world!");

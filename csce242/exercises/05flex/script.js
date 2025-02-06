c487ff1f0db472ba006b7d25352eae4ee957e5ab
cse-brownjessiej <jessiejb@email.sc.edu>
2025-02-06 16:49:10 -0500

    day6-3-25
    Took 25 seconds

--- csce242/exercises/05flex/script.js
+++ csce242/exercises/05flex/script.js
@@ -5,16 +5,26 @@
 // Version 2 Execution of anonymous code
 
 document.getElementById("btn-click-one").onclick = () => {
-   console.log("hello world!");
-   document.getElementById("message-one").innerHTML = "First Hello, world!";
+   document.getElementById("message-one").innerHTML = "be happy";
 }
 
 // Change color
 document.getElementById("btn-click-two").onclick = () => {
    const messageP = document.getElementById("message-two");
-   messageP.innerHTML = "Second Hello, world!";
-   messageP.classList.toggle("win");
+   messageP.innerHTML = "be sad";
+   messageP.classList.add("win");
+ }
 
+ document.getElementById("btn-click-three").onclick = () => {
+   document.getElementById("message-one").innerHTML = "";
+ }
+
+ document.getElementById("feeling").onkeydown = (event) => {
+      document.getElementById("feeling").innerHTML = 
+      "You are feeling: " + event.currentTarget.value;
+   
+  
+   
  }
 
  

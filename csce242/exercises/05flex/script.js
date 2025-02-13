2eb3b719e9cdd415cb2440143553a5c4a3ac398f
cse-brownjessiej <jessiejb@email.sc.edu>
2025-02-13 12:40:33 -0500

    2-13-25
    Took 6 minutes

--- csce242/exercises/05flex/script.js
+++ csce242/exercises/05flex/script.js
@@ -45,6 +45,67 @@ document.getElementById("btn-click-two").onclick = () => {
   `You choose ${color}. You are feeling ${mood}.`
  }
 
+ let pos = 0;
+ const changeCirclePos = (increment) => {
+  pos += increment;
+  document.getElementById("ball").style.setProperty("top", pos + "px");
+ }
+
+ document.getElementById("down-btn").onclick = () => {
+  console.log("down button clicked");
+  pos++;
+  changeCirclePos(10);
+
+ }
+
+ document.getElementById("up-btn").onclick = () => {
+  console.log("up button clicked");
+  pos--;
+  changeCirclePos(-10);
+
+ }
+
+ document.getElementById("pick-btn").onchange = (event) => {
+  const color = event.currentTarget.value;
+  //document.getElementById("ball").style.setProperty("background-color", color);
+  document.getElementById("ball").style.setProperty("--ball-color", color);
+
+ }
+
+
+ const GOAL = 1000;
+ let donations = 0;
+
+ document.getElementById("goal-display").innerHTML = `Goal $${GOAL}`;
+    const donation = document.getElementById("donate").value;
+    document.getElementById("donation-error").classList.add("hidden");
+
+    if (isNaN(donation) || donation <= 0) {
+      document.getElementById("donation-error").classList.remove("hidden");
+      return;
+    }
+
+    // update donations
+    donations += donation;
+
+    // show donation message
+    document.getElementById("donation-message").innerHTML = `$${GOAL - donations} to go!`;
+    
+    // update the progress bar
+    document.getElementById("in-box").style.setProperty("--donation-percent", donationPercent + "%");
+
+    // reset the input
+   
+
+    // check if the goal is reached
+    
+
+ // on page load hide the error
+ 
+
+ // when add clicked if donation is negative display the error
+ // otherwise update the donations and display in console;
+
  
 
 

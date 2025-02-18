9e226117df5d3608ab906716e701483a1ce07e90
cse-brownjessiej <jessiejb@email.sc.edu>
2025-02-17 19:45:02 -0500

    2-14-25
    Took 2 hours 3 minutes

--- csce242/projects/part3/script.js
+++ csce242/projects/part3/script.js
@@ -1,3 +1,26 @@
-document.getElementById("toggle-nav").onclick = () => {
-    document.getElementById("nav-items").classList.toggle("hide-small");
-};
+/*Project Part:3*/
+/*Beginning JavaScript*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleNav = document.getElementById("toggle-nav");
+    const mainNav = document.getElementById("main-nav");
+   
+   toggleNav.addEventListener("click", function(event) {
+       event.stopPropagation();
+       if (mainNav.style.transform === "translateX(0%)") {
+           mainNav.style.transform = "translateX(-100%)";
+       } else {
+           mainNav.style.transform = "translateX(0%)";
+       }
+
+   document.addEventListener("click", function (event) {
+       if (!toggleNav.contains(event.target) && !mainNav.contains(event.target)) {
+           mainNav.style.transform = "translateX(-100%)";  // Close the navigation menu when clicked outside of it.
+       }
+   });
+   });
+});
+
+
+
+    

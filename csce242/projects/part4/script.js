2e3a1011dc1937a55ca9f50eac95b70842ac9115
cse-brownjessiej <jessiejb@email.sc.edu>
2025-02-17 23:50:09 -0500

    index.html
    Took 1 hour 7 minutes

---
+++ csce242/projects/part4/script.js
@@ -0,0 +1,9 @@
+/*Project Part:3*/
+/*Beginning JavaScript*/
+
+document.getElementById("toggle-nav").onclick = () => {
+    document.getElementById("nav-items").classList.toggle("hide-small");
+}
+
+
+    

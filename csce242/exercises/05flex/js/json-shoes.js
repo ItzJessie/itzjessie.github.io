042b1077df4d24b7644f8ddfe3293498b972a142
cse-brownjessiej <jessiejb@email.sc.edu>
2025-03-04 09:45:53 -0500

    json file

--- csce242/exercises/05flex/js/json-shoes.js
+++ csce242/exercises/05flex/js/json-shoes.js
@@ -13,7 +13,37 @@ const getShoes = async () => {
 
 const showShoes = async() => {
     const shoes = await getShoes();
-    console.log(shoes);
+    const shoeSection = document.getElementById("shoes-section");
+    
+    
+    shoes.forEach((shoe) => {
+        const section = document.createElement("section");
+        shoeSection.append(section);
+        const h3 = document.createElement("h3");
+        h3.innerHTML = shoe.name;
+        section.append(h3);
+
+        const p = document.createElement("p");
+        section.append(p);
+        p.innerHTML = `Brand: ${shoe.brand}`;
+
+
+        const ul = document.createElement("ul");
+        section.append(ul);
+
+
+        shoe.reviews.forEach((review)=> {
+            const li = document.createElement("li");
+            li.append(review);
+            ul.append(li);
+        });
+       
+    });
+
 };
+
+
+
+
 
 showShoes();

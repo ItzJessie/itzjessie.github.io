c12b219a583ef261b0e8e77862bbc2c75b9e4df5
cse-brownjessiej <jessiejb@email.sc.edu>
2026-02-08 22:26:58 -0500

    feb9

--- csce243/examples/04-javascript/script.js
+++ csce243/examples/04-javascript/script.js
@@ -1,22 +1,29 @@
-const bounceBall = () => {
-   const ballElement = document.getElementById("ball");
-   ballElement.classList.add("bounce");
+// const bounceBall = () => {
+//    const ballElement = document.getElementById("ball");
+//    ballElement.classList.add("bounce");
+// };
+
+// const writeMessage = () => {
+//     const firstName = document.getElementById("txt-first-name").value;
+//     const messageP = document.getElementById("message");
+
+//     messageP.innerHTML = "Hello " + firstName;
+// };
+
+
+
+// // Call the variable when the window loads not the function itself
+// window.onload = () => {
+//     document.getElementById("bounce-button").onclick = bounceBall;
+//     document.getElementById("message-button").onclick = writeMessage;
+// }; 
+
+
+// link click example
+document.getElementById("a-click!").onclick = (e) => {
+    e.preventDefault();
+    e.currentTarget.innerHTML = "Clicked!!!";
 };
-
-const writeMessage = () => {
-    const firstName = document.getElementById("txt-first-name").value;
-    const messageP = document.getElementById("message");
-
-    messageP.innerHTML = "Hello " + firstName;
-};
-
-
-
-// Call the variable when the window loads not the function itself
-window.onload = () => {
-    document.getElementById("bounce-button").onclick = bounceBall;
-    document.getElementById("message-button").onclick = writeMessage;
-}; 
 
 
 
